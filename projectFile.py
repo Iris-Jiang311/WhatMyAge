aaf85a006be3e0e88a9e6a8d5e72218eeb69ffd5
Iris Jiang <jiang123yonglin@126.com>
2025-02-09 14:23:38 +0100

    add function in 10 years

--- projectFile.py
+++ projectFile.py
@@ -1,9 +1,10 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 age_in_5_years = age + 5
-
+age_in_10_years = age + 10
 
 
 print(f"Hello {name}!")
 print(f"You are {age} years old.")
 print(f"In 5 years, you will be {age_in_5_years} years old.")
+print(f"In 10 years, you will be {age_in_10_years} years old.")
