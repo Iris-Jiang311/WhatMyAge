1a94e9a3f0e94668faf34a12214512b04db4dac5
Iris Jiang <jiang123yonglin@126.com>
2025-02-09 14:14:59 +0100

    initial commit

---
+++ projectFile.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+age_in_5_years = age + 5
+
+
+
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+print(f"In 5 years, you will be {age_in_5_years} years old.")
